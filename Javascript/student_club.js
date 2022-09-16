82f1bb7d00e0055aca86c1743a578555f5a59964
MostlyRice <amazinzygy@gmail.com>
2022-09-16 11:24:15 -0500

    Added button logic to add students

--- Javascript/student_club.js
+++ Javascript/student_club.js
@@ -3,7 +3,10 @@ let studentCount = document.querySelector('#student-count')
 let studentName = document.querySelector('#student-name')
 let studentID = document.querySelector('#student-id')
 let studentGPA = document.querySelector('#student-gpa')
+let studentList = document.querySelector('#student-list')
 let addStudent = document.querySelector('#add-student')
+let studentsInList = 0
+
 // Event listener for the saveButton
 addStudent.addEventListener('click', function() {
 // Reads the text in the input
@@ -11,7 +14,35 @@ let studentNameText = studentName.value
 let studentIDText = studentID.value
 let studentGPAText = studentGPA.value
 
-console.log(studentNameText, studentIDText, studentGPAText)
+// validation
+if (studentNameText  == '' ){
+    alert('Enter a name.')
+    return
+  }
+
+  if (studentIDText  == '' ){
+    alert('Enter your Student ID.')
+    return
+  }
+
+  if (studentGPAText  >= 4 || studentGPAText  == ''){
+    alert('GPA range has to be between 0-4.')
+    return
+  }
+
+// Create a new li item
+let listItem = document.createElement('li')
+// The li element text should be the text from the input
+listItem.innerHTML = ' ' + studentNameText + ' ' + studentIDText + ' ' + studentGPAText
+// Add the new li element as a child of the ul element
+studentList.appendChild(listItem)
+// Clears the text in the input
+studentName.value = ''
+studentID.value = ''
+studentGPA.value = ''
+// Adds to the counter
+studentsInList++
+studentCount.innerHTML = studentsInList
 })
     /* Add the following functionality to this page
 
